7cec138c7ead0b2beb6fddf176a216229c6f44de
faris <mrfaristp@gmail.com>
2024-01-31 23:40:48 +0530

    input ref added

--- frontend/src/components/homePage/NavBar.tsx
+++ frontend/src/components/homePage/NavBar.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useRef } from "react";
+
 const NavBar = () => {
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (searchInput.current) {
+      searchInput.current.focus();
+    }
+  }, []);
+
   return (
     <div>
       <div className="flex justify-between">
         <h1 className="py-2 text-4xl font-extrabold">Chats</h1>
       </div>
-      <input
-        type="search"
-        className="w-full rounded-lg px-3 py-1.5 outline-none"
-        placeholder="Search people"
-      />
+      <div className="sm:mr-1.5">
+        <input
+          ref={searchInput}
+          type="search"
+          className="w-full rounded-lg px-3 py-1.5 outline-none "
+          placeholder="Search people"
+        />
+      </div>
     </div>
   );
 };
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,8 +3,8 @@ import NavBar from "../components/homePage/NavBar";
 export default function HomePage() {
   return (
     <>
-      <div className="grid grid-col-1 sm:grid-cols-4 sm:pl-2">
-        <nav className="p-1.5 sm:p-0">
+      <div className="grid grid-col-1 sm:grid-cols-4 p-1.5 sm:pl-2">
+        <nav className="">
           <NavBar />
         </nav>
         <aside className="sm:col-span-2 bg-gray-800">he</aside>
